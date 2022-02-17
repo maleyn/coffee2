f7974d4025df83de839d6e693eb77fd46347f504
maleyran <maleyran.ren@hotmail.fr>
2022-02-17 09:01:21 +0100

    ca marche ?

--- functions/bdd.php
+++ functions/bdd.php
@@ -2,7 +2,8 @@
 
 function bdd(){
 try {
-    $bdd = new PDO("mysql:dbname=coffee;host=mysql-69235-0.cloudclusters.net", "admin", "okgVEEPm");
+    $bdd = new PDO("mysql:dbname=coffee;host=mysql-69235-0.cloudclusters.net:11138", "admin", "okgVEEPm");
+    var_dump($bdd);
 }catch(PDOException $e){
     echo "connexion impossible " . $e->getMessage();
 }
